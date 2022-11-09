e0bd96029c8d81d2b1cd60d88b6256f2359c1ffd
Vincent Basile <vpbphone@gmail.com>
2022-11-08 21:32:11 -0600

    Initial commit and setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { MantineProvider, Text, Button, Stack } from "@mantine/core";
+import { MantineProvider, Text, Button, Stack,  } from "@mantine/core";
+import { RichTextEditor } from "@mantine/rte";
+import { DatePicker } from '@mantine/dates';
 import { theme } from "./theme";
 
 export default function App() {
+  const loremIpsum = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non provident, commodi ex, mollitia sed magni, quae labore quas quibusdam totam odit at perspiciatis pariatur optio accusantium deserunt ratione! Rerum, non."
+
   return (
     <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
       <Stack align="center" mt={50}>
         <Text size="xl" weight={500}>
-          Welcome to Mantine!
+          Mantine Sandbox!
         </Text>
+        <Text size="sm" color="yellow">{loremIpsum}</Text>
+        
+        <RichTextEditor></RichTextEditor>
+        <DatePicker label="Date:"></DatePicker>
         <Button>Click the button</Button>
       </Stack>
     </MantineProvider>
--- src/theme.ts
+++ src/theme.ts
@@ -2,4 +2,7 @@ import { MantineThemeOverride } from "@mantine/core";
 
 export const theme: MantineThemeOverride = {
   colorScheme: "dark",
+  focusRing: "always",
+  respectReducedMotion: true,
+  dateFormat: "YYYY-MM-DD"
 };
